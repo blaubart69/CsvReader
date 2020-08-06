03699bc1b1b6690f60bf49ce4c2f1826888568df
Bernhard Spindler <bernhard.spindler@unicredit.at>
2020-08-06 15:07:33 +0200

    Add project files.

---
+++ CsvReader/CsvReader.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace Spi
+{
+    public class CsvReader
+    {
+        public static void Run(TextReader reader, char fieldDelimiter, Action<string[]> OnRow)
+        {
+            List<string> fields = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            bool eof;
+            do
+            {
+                eof = ReadRow(reader, fieldDelimiter, ref fields, ref sb);
+                if (fields.Count > 0)
+                {
+                    OnRow(fields.ToArray());
+                }
+            }
+            while (!eof);
+        }
+
+        private const char DOUBLE_QUOTE = '"';
+        private static bool ReadRow(TextReader reader, char fieldDelimiter, ref List<string> fields, ref StringBuilder sb)
+        {
+            sb.Clear();
+            fields.Clear();
+
+            int int_c = reader.Read();
+            if (int_c == -1)
+            {
+                return true;
+            }
+
+            bool eof = false;
+            bool inQuotes = false;
+            char? lastChar = fieldDelimiter;
+
+            for (;;)
+            {
+                bool setLastChar = true;
+                char c = (char)int_c;
+
+                // -------------------------------------------------------------------------
+                if (c == DOUBLE_QUOTE)
+                // -------------------------------------------------------------------------
+                {
+                    if (lastChar == fieldDelimiter)
+                    {
+                        inQuotes = true;
+                        setLastChar = false;
+                    }
+                    else if (lastChar == DOUBLE_QUOTE)
+                    {
+                        sb.Append(DOUBLE_QUOTE);
+                        setLastChar = false;
+                    }
+                }
+                // -------------------------------------------------------------------------
+                else if (c == fieldDelimiter)
+                // -------------------------------------------------------------------------
+                {
+                    if (lastChar == DOUBLE_QUOTE)
+                    {
+                        inQuotes = false;
+                    }
+
+                    if (inQuotes)
+                    {
+                        sb.Append(fieldDelimiter);
+                    }
+                    else
+                    {
+                        fields.Add(sb.ToString());
+                        sb.Clear();
+                    }
+                }
+                // -------------------------------------------------------------------------
+                else if (c == '\n' || c == '\r')
+                // -------------------------------------------------------------------------
+                {
+                    if (inQuotes)
+                    {
+                        if ( lastChar == DOUBLE_QUOTE )
+                        {
+                            inQuotes = false;
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+                    }
+                    if ( !inQuotes && c == '\n')
+                    {
+                        fields.Add(sb.ToString());
+                        break;
+                    }
+                }
+                // -------------------------------------------------------------------------
+                else
+                // -------------------------------------------------------------------------
+                {
+                    sb.Append(c);
+                }
+
+                lastChar = setLastChar ? c : (char?)null;
+
+                int_c = reader.Read();
+                if (int_c == -1)
+                {
+                    eof = true;
+                    fields.Add(sb.ToString());
+                    break;
+                }
+            }
+
+            return eof;
+        }
+    }
+}
---
+++ TestCsvReader/TestCsvRead.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spi;
+
+namespace TestCsvReader
+{
+    [TestClass]
+    public class TestCsvRead
+    {
+        [TestMethod]
+        public void one_lonely_field()
+        {
+            var data = RunCsvReader(new StringReader("a"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(1, data[0].Length);
+            Assert.AreEqual("a", data[0][0]);
+        }
+        [TestMethod]
+        public void one_lonely_field_crlf()
+        {
+            var data = RunCsvReader(new StringReader("a\r\n"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(1, data[0].Length);
+            Assert.AreEqual("a", data[0][0]);
+        }
+        [TestMethod]
+        public void one_lonely_field_lf()
+        {
+            var data = RunCsvReader(new StringReader("a\n"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(1, data[0].Length);
+            Assert.AreEqual("a", data[0][0]);
+        }
+        [TestMethod]
+        public void Line1_Field2()
+        {
+            var data = RunCsvReader(new StringReader("a,b"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("a", data[0][0]);
+            Assert.AreEqual("b", data[0][1]);
+        }
+        [TestMethod]
+        public void Line1_Field2_ending_with_CrLf()
+        {
+            var data = RunCsvReader(new StringReader("a,b\r\n"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("a", data[0][0]);
+            Assert.AreEqual("b", data[0][1]);
+        }
+        [TestMethod]
+        public void Line2_Field2()
+        {
+            var data = RunCsvReader(new StringReader("a,b\r\nc,d"));
+            Assert.AreEqual(2, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual(2, data[1].Length);
+            Assert.AreEqual("a", data[0][0]);
+            Assert.AreEqual("b", data[0][1]);
+            Assert.AreEqual("c", data[1][0]);
+            Assert.AreEqual("d", data[1][1]);
+        }
+        [TestMethod]
+        public void Line1_Field2_quoted()
+        {
+            var data = RunCsvReader(new StringReader("\"a\",\"b\""));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("a", data[0][0]);
+            Assert.AreEqual("b", data[0][1]);
+        }
+        [TestMethod]
+        public void twoEmptyQuotedFields()
+        {
+            var data = RunCsvReader(new StringReader("\"\",\"\""));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("", data[0][0]);
+            Assert.AreEqual("", data[0][1]);
+        }
+        [TestMethod]
+        public void twoEmptyQuotedFields_CrLf_ending()
+        {
+            var data = RunCsvReader(new StringReader("\"\",\"\"\r\n"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("", data[0][0]);
+            Assert.AreEqual("", data[0][1]);
+        }
+        [TestMethod]
+        public void twoQuotedDoubleQuotes()
+        {
+            var data = RunCsvReader(new StringReader("\"\"\"\",\"\"\"\""));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("\"", data[0][0]);
+            Assert.AreEqual("\"", data[0][1]);
+        }
+        [TestMethod]
+        public void twoQuotedDoubleQuotes_literal()
+        {
+            var data = RunCsvReader(new StringReader(@""""""""","""""""""));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("\"", data[0][0]);
+            Assert.AreEqual("\"", data[0][1]);
+        }
+        [TestMethod]
+        public void twoQuotedDoubleQuotes_crlf()
+        {
+            var data = RunCsvReader(new StringReader("\"\"\"\",\"\"\"\"\r\n"));
+            Assert.AreEqual(1, data.Count);
+            Assert.AreEqual(2, data[0].Length);
+            Assert.AreEqual("\"", data[0][0]);
+            Assert.AreEqual("\"", data[0][1]);
+        }
+        private List<string[]> RunCsvReader(TextReader r)
+        {
+            List<string[]> data = new List<string[]>();
+
+            using (r)
+            {
+                Spi.CsvReader.Run(r, ',', row => data.Add(row) );
+            }
+
+            return data;
+        }
+    }
+}
