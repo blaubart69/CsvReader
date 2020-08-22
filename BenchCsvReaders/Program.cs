c08aad67ec27b247b7a017c7da616fbd86db0eaf
bee <bernhard.spindler.75@gmail.com>
2020-08-22 13:06:54 +0200

    rename to CsvReaderBuffer

--- BenchCsvReaders/Program.cs
+++ BenchCsvReaders/Program.cs
@@ -1,4 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
 using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 using System;
@@ -7,7 +9,9 @@ using System.IO;
 namespace BenchCsvReaders
 {
     [MemoryDiagnoser]
-    //[InliningDiagnoser]
+    //[InliningDiagnoser(logFailuresOnly: false, filterByNamespace: false)]
+    //[DisassemblyDiagnoser(maxDepth:3,printSource:true)]
+    //[HardwareCounters(HardwareCounter.BranchMispredictions, HardwareCounter.CacheMisses, HardwareCounter.BranchInstructions)]
     public class Benches
     {
         //[Benchmark]
@@ -44,7 +48,7 @@ namespace BenchCsvReaders
                 }
             }
         }
-        [Benchmark]
+        //[Benchmark]
         public void Spi_Reader_V3_dir_64k()
         {
             using (TextReader rdr = new StreamReader(@"c:\temp\allc.tsv", detectEncodingFromByteOrderMarks: true))
@@ -67,7 +71,7 @@ namespace BenchCsvReaders
                 }
             }
         }
-        [Benchmark]
+        //[Benchmark]
         public void NReco_dir_64k()
         {
             using (TextReader rdr = new StreamReader(@"c:\temp\allc.tsv", detectEncodingFromByteOrderMarks: true))
@@ -78,7 +82,7 @@ namespace BenchCsvReaders
                 }
             }
         }
-        [Benchmark]
+        //[Benchmark]
         public void Spi_Reader_V3_5MB_quoted_64k()
         {
             using (TextReader rdr = new StreamReader(@"c:\Users\bee\Downloads\2.csv", detectEncodingFromByteOrderMarks: true))
@@ -90,12 +94,35 @@ namespace BenchCsvReaders
             }
         }
 
-        [Benchmark]
+        //[Benchmark]
         public void NReco_5MB_quoted_64k()
         {
             using (TextReader rdr = new StreamReader(@"c:\Users\bee\Downloads\2.csv", detectEncodingFromByteOrderMarks: true))
             {
                 var csvrdr = new NReco.Csv.CsvReader(rdr, ";") { BufferSize = 64 * 1024, TrimFields=false };
+                while (csvrdr.Read())
+                {
+                }
+            }
+        }
+        [Benchmark]
+        public void Spi_Reader_V3_dir_quoted_32k()
+        {
+            using (TextReader rdr = new StreamReader(@"c:\temp\allc_quoted.csv", detectEncodingFromByteOrderMarks: true))
+            {
+                var csvrdr = new Spi.CsvReader3(rdr, ',', buffersize: 32 * 1024);
+                while (csvrdr.Read())
+                {
+                }
+            }
+        }
+
+        [Benchmark]
+        public void NReco_5MB_dir_quoted_32k()
+        {
+            using (TextReader rdr = new StreamReader(@"c:\temp\allc_quoted.csv", detectEncodingFromByteOrderMarks: true))
+            {
+                var csvrdr = new NReco.Csv.CsvReader(rdr, ",") { TrimFields = false };
                 while (csvrdr.Read())
                 {
                 }
--- CsvReaderBuffer/CsvReader3.cs
+++ CsvReaderBuffer/CsvReader3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace Spi
 {
