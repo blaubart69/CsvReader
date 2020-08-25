8d04c47e6b58595c47329d1f533426c65189ff5b
Bernhard Spindler <bernhard.spindler@unicredit.at>
2020-08-25 14:27:11 +0200

    benchmarks fixed

--- BenchCsvReaders/Program.cs
+++ BenchCsvReaders/Program.cs
@@ -14,7 +14,7 @@ namespace BenchCsvReaders
     //[HardwareCounters(HardwareCounter.BranchMispredictions, HardwareCounter.CacheMisses, HardwareCounter.BranchInstructions)]
     public class Benches
     {
-        [Benchmark]
+        //[Benchmark]
         public void Spi_Reader_V3_dir__4k()
         {
             using (TextReader rdr = new StreamReader(@"c:\temp\allc.tsv", detectEncodingFromByteOrderMarks: true))
@@ -69,30 +69,30 @@ namespace BenchCsvReaders
                 }
             }
         }
-        //[Benchmark]
-        public void Spi_Reader_V3_5MB_quoted_32k()
+        [Benchmark]
+        public void Spi_Reader_V4_5MB_quoted_64k()
         {
-            using (TextReader rdr = new StreamReader(@"c:\Users\bee\Downloads\2.csv", detectEncodingFromByteOrderMarks: true))
+            using (TextReader rdr = new StreamReader(@"c:\temp\2.csv", detectEncodingFromByteOrderMarks: true))
             {
-                var csvrdr = new Spi.CsvReader3(rdr, ';', buffersize: 32*1024);
+                var csvrdr = new Spi.CsvReader4(rdr, ';', buffersize: 64*1024);
                 while (csvrdr.Read())
                 {
                 }
             }
         }
 
-        //[Benchmark]
-        public void NReco_5MB_quoted_32k()
+        [Benchmark]
+        public void NReco_5MB_quoted_64k()
         {
-            using (TextReader rdr = new StreamReader(@"c:\Users\bee\Downloads\2.csv", detectEncodingFromByteOrderMarks: true))
+            using (TextReader rdr = new StreamReader(@"c:\temp\2.csv", detectEncodingFromByteOrderMarks: true))
             {
-                var csvrdr = new NReco.Csv.CsvReader(rdr, ";") { TrimFields=false };
+                var csvrdr = new NReco.Csv.CsvReader(rdr, ";") { TrimFields=false, BufferSize=64*1024 };
                 while (csvrdr.Read())
                 {
                 }
             }
         }
-        [Benchmark]
+        //[Benchmark]
         public void Spi_Reader_V3_dir_quoted_32k()
         {
             using (TextReader rdr = new StreamReader(@"c:\temp\allc_quoted.csv", detectEncodingFromByteOrderMarks: true))
